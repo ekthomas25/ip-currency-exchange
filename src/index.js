d382ce53c5da8f230569d435c025aadc605a32d9
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-19 15:58:32 -0800

    Add conditional to disallow zero input in text field

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ function clearOutput() {
   $('#amnt').val("");
 }
 
+
 $(document).ready(function() {
   $('#currencyBtn').click(function() {
     let code = $('#currency').val();
@@ -18,8 +19,12 @@ $(document).ready(function() {
     let promise = CurrencyService.getRate(code, amount);
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $('.showRate').text(`${body.conversion_result} `);
-      $('.currencyErr').hide();
+      if (typeof body.conversion_result !== 'number') {
+        $('.showErrors').text(`Please enter a number greater than zero`);
+      } else {
+        $('.showRate').text(`${body.conversion_result} `);
+        $('.currencyErr').hide();
+      }
     }, function(error) {
       const errorType = JSON.parse(error);
       $('.showErrors').text(`There was an error processing your request. Error: ${errorType["error-type"]}`);
