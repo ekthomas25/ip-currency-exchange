e9854d1ecfcc291076f159d136a7571a94a150e9
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-11-19 12:57:15 -0800

    remove commented out fetch api

--- src/js/CurrencyService.js
+++ src/js/CurrencyService.js
@@ -1,15 +1,5 @@
 export default class CurrencyService {
   static getRate(code, amount) {
-    // return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${code}/${amount}`)
-    //   .then(function(response) {
-    //     if (!response.ok) {
-    //       throw Error(response.statusText);
-    //     }
-    //     return response.json();
-    //   })
-    //   .catch(function(error) {
-    //     return error;
-    //   });
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${code}/${amount}`;
